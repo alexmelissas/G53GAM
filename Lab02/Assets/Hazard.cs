f18ac954f35e25478641148549da4946008bfb37
Alex <psyam19@nottingham.ac.uk>
2020-02-10 17:32:28 +0000

    still cant get fall detection right

---
+++ Lab02/Assets/Hazard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    public AnimationCurve curve;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            curve.Evaluate((Time.time % curve.length)),
+            transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
