285b87fb78a31ce8169c8fd11ceb8bbf1d030e95
Alex <psyam19@nottingham.ac.uk>
2020-02-29 17:19:14 +0000

    lab4 done

---
+++ Lab04/Assets/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public Door doorObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorObject.Open();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorObject.Close();
+    }
+}
---
+++ Lab04/Assets/wind.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wind : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody b = other.gameObject.GetComponent<Rigidbody>();
+        b.AddForce(new Vector3(0, 50, 0), ForceMode.Force);
+    }
+}
