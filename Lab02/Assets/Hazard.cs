1a25b7480c77ce38d399d0e9820067990728781c
Alex <psyam19@nottingham.ac.uk>
2020-04-12 21:36:42 +0100

    lab02 extras done

--- Lab02/Assets/Hazard.cs
+++ Lab02/Assets/Hazard.cs
@@ -9,28 +9,21 @@ public class Hazard : MonoBehaviour
 
     public AnimationCurve curve;
 
+    private float x,y,z;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (vertical)
-        {
-            transform.position = new Vector3(transform.position.x,
-            curve.Evaluate((Time.time % curve.length)),
-            transform.position.z);
-        }
-        else
-        {
-            transform.position = new Vector3(curve.Evaluate((Time.time % curve.length)),
-            transform.position.y,
-            transform.position.z);
-        }
-        
+        if (vertical) transform.position = new Vector3(x, y + curve.Evaluate((Time.time % curve.length)), z);
+        else transform.position = new Vector3(x + curve.Evaluate((Time.time % curve.length)), y, z);        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
