8cda6b7e74cf5a791a931f1610d4c8471f05a8be
Alex <psyam19@nottingham.ac.uk>
2020-04-13 09:25:57 +0100

    Lab04 extras done

---
+++ Lab04/Assets/disableWin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class disableWin : MonoBehaviour
+{
+    public Text winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Lab04/Assets/winPad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class winPad : MonoBehaviour
+{
+    public Text winText;
+    private void OnTriggerEnter(Collider other)
+    {
+        winText.enabled = true;
+    }
+}
